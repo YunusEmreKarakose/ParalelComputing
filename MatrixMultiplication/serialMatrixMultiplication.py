676f5ec40d5a1e7075e5f348f51ee516bcfa3410
Yunus Emre Karaköse <yemre05@hotmail.com.tr>
2020-05-21 02:50:06 +0300

    Create serialMatrixMultiplication.py

---
+++ MatrixMultiplication/serialMatrixMultiplication.py
@@ -0,0 +1,46 @@
+#numpy for double
+import numpy as np
+import time as time
+# AxB=C
+def matrixMul(A,B):
+    #A matrix
+    rowCountA=len(A)
+    colCountA=len(A[0])
+    #B Matrix
+    rowCountB=len(B)
+    colCountB=len(B[0])
+
+    if(colCountA != rowCountB):
+        print("Can't multiply A and B")
+        return
+    #create C array(result) colContB x rowCountA
+    C=[[0 for row in range(colCountB)] for col in range(rowCountA)]
+
+    for i in range(rowCountA):
+        for j in range(colCountB):
+            tmp=0
+            for k in range(colCountA):
+                tmp=tmp+(A[i][k]*B[k][j])
+            C[i][j]=tmp
+    
+    #print(C)
+    return
+
+# run    
+N=int(200)
+
+#float
+matrixA=[[np.float32(1.0) for row in range(N)] for col in range(N)]
+matrixB=[[np.float32(1.0) for row in range(N)] for col in range(N)]
+"""
+#double
+matrixA=[[np.float64(1.0) for row in range(N)] for col in range(N)]
+matrixB=[[np.float64(1.0) for row in range(N)] for col in range(N)]
+"""
+#multiplication start time
+startTime=time.time()
+
+matrixMul(matrixA,matrixB)
+
+
+print("multiplication has done--- %s seconds ---" % (time.time() - startTime))
